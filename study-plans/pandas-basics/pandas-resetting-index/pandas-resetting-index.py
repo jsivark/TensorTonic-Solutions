e0bd49795d57052572f199822edbd16490d258d0
siva <65615807+jsivark@users.noreply.github.com>
2026-05-18 17:34:09 +0530

    Add solution for pandas-resetting-index

---
+++ study-plans/pandas-basics/pandas-resetting-index/pandas-resetting-index.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def reset_index_demo(data, index_col):
+    """
+    Returns: list [columns_before_reset, columns_after_reset]
+    """
+    df = pd.DataFrame(data)
+    
+    df = df.set_index(index_col)
+
+    columns_before_reset = df.columns.tolist()
+    
+    df = df.reset_index()
+
+    columns_after_reset = df.columns.tolist()
+
+    return [columns_before_reset, columns_after_reset]
